4d57f702e2c8557ceedbeaccff8e312d7db6a52c
Sceno Ichiro <sceno.ichiro@gmail.com>
2025-12-04 16:42:04 +0900

    Update notion.js

--- scripts/notion.js
+++ scripts/notion.js
@@ -1,19 +1,18 @@
 import { Client } from "@notionhq/client"
-import fs from "fs"
 
 const notion = new Client({
   auth: process.env.NOTION_TOKEN,
 })
 
-async function getDatabase(databaseId, outputFile) {
+const databaseId = process.env.WORKS_DB
+
+async function getDatabase() {
   const response = await notion.databases.query({
     database_id: databaseId,
   })
 
-  fs.writeFileSync(outputFile, JSON.stringify(response.results, null, 2))
-  console.log(`${outputFile} generated`)
+  console.log("✅ Success! データ取得できました")
+  console.log(response.results)
 }
 
-(async () => {
-  await getDatabase(process.env.WORKS_DB, "./data/works.json")
-})()
+getDatabase()
